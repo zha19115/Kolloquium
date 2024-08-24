fed704dbf617790b123bdb5d4f31cecf5891230a
ANNI_ZHAN <zha19115@spengergasse.at>
2024-08-24 21:10:26 +0200

    Entities
    +AppDbContext

---
+++ Kolloquium/Models/Entities/Customer.cs
@@ -0,0 +1,11 @@
+﻿namespace Kolloquium.Models.Entities
+{
+    public class Customer
+    {
+        public Guid c_id { get; set; }
+        public string c_name { get; set; }
+        public string c_phone { get; set; }
+        public string c_email { get; set; }
+        public bool c_newsletter { get; set; }
+    }
+}
